ef0bc8985bb5fe52b78fb22b290d22f774176e6a
Eseoghene-ODODO <en.ododohotmail.com>
2023-01-05 15:04:05 -0800

    Write a program that prints #pythoniscool, followed by a new line, in the standard output.

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys, math
     result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
+    for i in sys.argv:
+        result += int(i)
         print("{}".format(result)) 
