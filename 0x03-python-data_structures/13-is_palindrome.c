003c34e7fe3e2f2a9ba250332830e72856975dd4
Eseoghene-ODODO <en.ododohotmail.com>
2023-01-09 16:16:50 -0800

    Write a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,53 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-	if (head == NULL|| *head == NULL)
+	if (*head == NULL || head2->next == NULL)
 		return (1);
-
-	temp = *head;
-	while (temp) /* get len of list */
+	while (head2 != NULL)
 	{
-		temp = temp->next;
-		len++;
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
 	}
-	return (0);
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
