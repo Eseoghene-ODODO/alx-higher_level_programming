fd101b994bb503029a2178b4a39472f209aa0e38
Eseoghene-ODODO <en.ododohotmail.com>
2023-02-01 12:06:52 -0800

    Solution to task 0-5

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 | self.__height == 0:
             return 0
-        return (2 * (self.__height + self.__width))
+        return ((2 * self.__height) + (2 * self.__width))
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 | self.__height == 0:
             return 0
-        return (2 * (self.__height + self.__width))
+        return ((2 * self.__height) + (2 * self.__width))
 
     def __str__(self):
         if self.__width == 0 | self.__height == 0:
