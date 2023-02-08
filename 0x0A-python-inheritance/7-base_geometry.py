e08252938b0337753508835ca11e57e5481b04c4
Eseoghene-ODODO <en.ododohotmail.com>
2023-02-08 01:40:25 -0800

    Solution to task 0-8

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
         """Public instance method to validate value is an int"""
 
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
