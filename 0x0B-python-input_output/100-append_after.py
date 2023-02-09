2d1913d5aa02f76f74b1abd03b949b68bec57b67
Eseoghene-ODODO <en.ododohotmail.com>
2023-02-09 04:16:23 -0800

    Solution to task 0-101

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,8 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """append function initialization"""
+
     with open(filename, "r+", encoding="utf-8") as file_pos:
         lines = [line for line in file_pos]
         for idx, line in enumerate(lines):
