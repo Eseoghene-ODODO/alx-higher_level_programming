9155c1e72fff56d29d5f033543271039470a38ca
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-04 19:22:46 +0000

    corrected version for task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 from add_0 import add
-"""
-Python script that imports a function called add from add_0
-Arguments:
+if __name__ ==  '__main__':
+    """
+        Python script that imports a function called add from add_0
+        Arguments:
+            a = 1
+            b = 2
+            print('{} + {} = {}'. format(a, b, add(a, b))
+        final result becomes 1 + 2 = 3
+    """
+
+
     a = 1
     b = 2
-    print('{} + {} = {}'. format(a, b, add(a, b))
-final result becomes 1 + 2 = 3
-"""
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
