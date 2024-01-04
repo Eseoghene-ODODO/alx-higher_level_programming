eed03b13944b73ba991160176f69b35964571abe
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-04 19:13:00 +0000

    Task 0 solved

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from add_0 import add
+"""
+Python script that imports a function called add from add_0
+Arguments:
+    a = 1
+    b = 2
+    print('{} + {} = {}'. format(a, b, add(a, b))
+final result becomes 1 + 2 = 3
+"""
+
+
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
