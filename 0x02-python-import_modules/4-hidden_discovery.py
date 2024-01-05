f42d086469388a3e22cd53ef1a73c9840aa9e5fe
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-05 01:12:55 +0100

    corrected task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,21 +4,21 @@ import importlib.util
 import uncompyle6
 
 if __name__ == "__main__":
+    # Decompile pyc file
+    with open('hidden_4_decompiled.py', 'w') as f:
+        uncompyle6.decompile_file('hidden_4.pyc', f)
 
-  # Decompile pyc file
-  with open('hidden_4_decompiled.py', 'w') as f:
-    uncompyle6.decompile_file('hidden_4.pyc', f)
+    # Import decompiled module
+    spec = importlib.util.spec_from_file_location('hidden_4',
+                                                  'hidden_4_decompiled.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
-  # Import decompiled module
-  spec = importlib.util.spec_from_file_location('hidden_4', 'hidden_4_decompiled.py')
-  module = importlib.util.module_from_spec(spec)
-  spec.loader.exec_module(module)
+    # Print names from module
+    names = set()
+    for name in dir(module):
+        if not name.startswith("__"):
+            names.add(name)
 
-  # Print names from module
-  names = set()
-  for name in dir(module):
-    if not name.startswith("__"):
-      names.add(name)
-
-  for name in sorted(names):
-    print(name)
+    for name in sorted(names):
+        print(name)
