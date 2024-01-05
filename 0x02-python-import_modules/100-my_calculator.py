299cfa37076d6f0127efe67e56fb5c767b162bc4
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-05 03:06:08 +0100

    solved with some errors in 6 and 4

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,22 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
+    operators = ['+', '-', '*', '/'] 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-
-    if operator == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
+    elif argv[2] not in operators:
         print("Unknown operatgor. Available operator: +, -, * and /")
         exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+
+        if argv[2] == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif argv[2] == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
