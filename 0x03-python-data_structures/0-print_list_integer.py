1dacf6cf48d88951b76883d071f5e1cc1d7aa326
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-08 00:43:15 +0100

    Task 0 resolved

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Function that prints integers in an horizontal line
+"""
+
+
 def print_list_integer(my_list=[]):
+    """ The function that ptints the content of a list """
     for i in my_list:
         print("{}".format(i))
