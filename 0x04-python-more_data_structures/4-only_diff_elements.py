022c8dd74e954c7cd4b42035639f2c9fc915e407
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-10 23:57:45 +0000

    Task 4 solved

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A function that returns a set of all elements present in only one set
+"""
+
+
+def only_diff_elements(set_1, set_2):
+    """ Function body """
+    diff_elements = set()
+    for element in set_1:
+        if element not in set_2:
+            diff_elements.add(element)
+        for elem in set_2:
+            if elem not in set_1:
+                diff_elements.add(elem)
+    return diff_elements
