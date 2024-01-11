9c2087060336316c81f74f951c0f6622182d9385
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-11 04:50:44 +0000

    Task 12 resolved

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """
-A function that converts a roman numeral to an integer
+A function that converts roman numerals to integers
 """
 
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-  number = 0
-  i = 0
 
-  while i < len(roman_string):
-      first_char = roman_numerals[roman_string[i]]
-    if i + 1 < len(roman_string):
-        next_char = roman_numerals[roman_string[i + 1]]
-      if first_char < next_char:
-          number -= first_char
-      else:
-          number += first_char
-    else:
-        number += first_char
-    i += 1
+    roman_numerals = {
+            'I': 1, 'V': 5,
+            'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            }
+    result = 0
 
-  return number
+    for i in range(len(roman_string)):
+        current_value = roman_numerals[roman_string[i]]
+        next_value = roman_numerals[roman_string[i + 1]]
+        if i + 1 < len(roman_string) else 0
+
+        if current_value < next_value:
+            result += (next_value - current_value)
+            i += 1  # Skip the next character
+
+        else:
+            result += current_value
+
+    return result
