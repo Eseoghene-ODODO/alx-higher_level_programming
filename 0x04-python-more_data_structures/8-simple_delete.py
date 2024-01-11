444f0462ec9c43877e5ec4b5976efecc664d4bcb
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-11 01:33:57 +0000

    Task 8 solved

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that deletes a key in a dictionary
+"""
+
+
+def simple_delete(a_dictionary, key=""):
+    """ Function body """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
