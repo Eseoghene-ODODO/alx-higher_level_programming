313d1de1caafda1910173c4cdb2467830bcc320d
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-22 22:32:30 +0000

    task 100 completed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,8 +7,9 @@ A function that prints an integer.
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
-        return True
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
     except ValueError as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
