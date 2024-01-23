785506ca6c7be043d59d83525864f8fe83ff208e
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-23 12:02:34 +0000

    task 1 solved

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A Class Square that defines a square
+"""
+
+
+class Square:
+    """ class body """
+
+    def __init__(self, size):
+        """ instance of a class """
+        self.__size = size
