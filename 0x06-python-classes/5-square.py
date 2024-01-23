a1ad58f74529ff7ac2f7d405f65d54adabcf0503
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-23 13:46:25 +0000

    task 5 solved

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A Class Square that defines a square
+"""
+
+
+class Square:
+    """ class body """
+
+    def __init__(self, size=0):
+        """ instance of a class """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ property body """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter body """
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ method body """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ my print body """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
