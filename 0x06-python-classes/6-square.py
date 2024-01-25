40168f5dbcc8f6f9151ea9bdc21239f031422d43
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-25 08:03:10 +0100

    6 done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A Class Square that defines a square
+A class Square that defines a square
 """
 
 
@@ -8,48 +8,54 @@ class Square:
     """ class body """
 
     def __init__(self, size=0, position=(0, 0)):
-        """ instance of a class """
+        """Initialize private size and position attributes"""
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
-        """ property body """
+        """Getter for retrieving private size attribute"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ setter body """
-        self.__size = value
+        """Setter for validating and updating private size attribute"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
-        """ property body """
+        """Getter for retrieving private position attribute"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """ setter body """
-        self.__position = value
+        """Setter for validating and updating private position attribute"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(i, int) and i >= 0 for i in value):
+        if not all(isinstance(num, int) and num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
-        """ method body """
-        return self.__size * self.__size
+        """Calculate and return the current square area"""
+        return self.__size ** 2
 
     def my_print(self):
-        """ my print body """
+        """Print the square using #'s"""
+
         if self.__size == 0:
+            # Handle case of 0 size
             print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            return
+        # Print new lines based on position[1]
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            # Print spaces based on position[0]
+            print(" " * self.__position[0], end="")
+            # Print # based on size
+            print("#" * self.__size)
