aba483ea6ee3b84f2d442735048c0f2113b0e819
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-29 12:27:52 +0000

    task 1 done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,15 +9,18 @@ class Rectangle:
         Rectangle body
     """
     def __init__(self, width=0, height=0):
+        """ initializing the width and height values """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """ retrieving the width property """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ setting the the width value """
         self.__width = value
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -26,10 +29,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """ retrieving height property """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ setting the height value and checking for errors """
         self.__height = value
         if value < 0:
             raise ValueError("height must be >= 0")
