80a7d7290d602bcf46321b6195a4f8f5fe0c69fc
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-02 10:06:17 +0000

    test case

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,8 +8,6 @@ def say_my_name(first_name, last_name=""):
     """ function body """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if not isinstance((first_name, last_name), str):
-        raise TypeError("first_name or last_name must be a string")
     print(f"My name is {first_name} {last_name}")
