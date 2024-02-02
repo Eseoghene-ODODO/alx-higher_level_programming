99b8ed4aa026243bb59ffcee2860c8ea60de37a0
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-02 09:39:34 +0100

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,26 +5,26 @@ A that divides all elements of a matrix
 
 
 def matrix_divided(matrix, div):
-    """
-    Divides all elements of a matrix by div
+    """Divide all elements of a matrix.
     Args:
-        matrix
-        div
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
     Raises:
-        TypeError
-    Rturns:
-        A division of a list of a list
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-        
+    if not isinstance(matrix, list) or matrix == [] or \
+            not all(isinstance(row, list) for row in matrix):
+        raise TypeError('matrix must be a matrix (list of lists) \
+                        of integers/floats')
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError('Each row of the matrix must have the same size')
-        
     if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
-        
     if div == 0:
         raise ZeroDivisionError('division by zero')
-        
     return [[round(element / div, 2) for element in row] for row in matrix]
