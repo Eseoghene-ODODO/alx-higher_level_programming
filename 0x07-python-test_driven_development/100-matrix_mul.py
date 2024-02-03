c511b230f2b8b2bb9ac13691ec9339a054bb1dd2
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-03 02:53:26 +0000

    test

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A function that multiplies 2 matrices
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """ function body """
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("m_a must be a list or m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+
+    if not m_a or not m_b or not all(m_a) or not all(m_b):
+        raise ValueError("m_a can't be empty or m_b can't be empty")
+
+    if not all(isinstance(element, (int, float)) for row in m_a for element in row) or not all(isinstance(element, (int, float)) for row in m_b for element in row):
+        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+
+    if len(set(len(row) for row in m_a)) > 1 or len(set(len(row) for row in m_b)) > 1:
+        raise TypeError("Each row of m_a must be of the same size or each row of m_b must be of the same size")
+
+    # Check if matrices can be multiplied
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    # Perform matrix multiplication
+    result = [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*m_b)] for row_a in m_a]
+
+    return result
