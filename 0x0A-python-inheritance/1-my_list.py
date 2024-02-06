75a55fa7f7a9b726ede3679f284abd1a6785828d
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-06 07:37:45 +0000

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,13 +7,6 @@ A class MyList that inherits from list
 class MyList(list):
     """ class body """
 
-    def __init__(self, initial_list):
-        """ init body """
-        for element in initial_list:
-            if not isinstance(element, int):
-                raise TypeError("elements must be an integer")
-        return super().__init__(initial_list)
-
     def print_sorted(self):
         """ method body """
         sorted_list = sorted(self)
