fbb9887a893ed127a911689445305bbc8f4d0dea
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-06 00:32:10 +0000

    task 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ class body """
+    
+    def area(self):
+        """ method body """
+        raise Exception("area() is not implemented")
