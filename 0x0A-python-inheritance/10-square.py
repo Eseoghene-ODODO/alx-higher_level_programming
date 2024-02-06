c17ead6f1d97cc0dd22dbc48b1e801cf28f9058e
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-06 08:59:56 +0000

    rest

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,45 +1,9 @@
 #!/usr/bin/python3
 """
-A class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+A class Square that inherits from Rectangle (9-rectangle.py)
 """
 
-
-class BaseGeometry:
-    """ class body """
-
-    def area(self):
-        """ method body """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ method body """
-        if not isinstance(name, str):
-            raise TypeError(f"{name} must be a string")
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        self.name = name
-        self.value = value
-
-
-class Rectangle(BaseGeometry):
-    """ class body """
-
-    def __init__(self, width, height):
-        """ method body """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """ method body """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ method body """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -48,8 +12,9 @@ class Square(Rectangle):
     def __init__(self, size):
         """ method body """
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
-    def _str__(self):
+    def area(self):
         """method body """
-        return f"[Square] {self._Rectangle.__width}/{self._Rectangle.__height}"
+        return self.__size ** 2
