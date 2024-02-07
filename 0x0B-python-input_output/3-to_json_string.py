5a2bee00c5d2d0c8308338d2461c8d7cf0873d89
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-07 23:22:59 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that returns the JSON representation of an object (string)
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """function body"""
+    return json.dumps(my_obj)
