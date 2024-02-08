b1ba0bc7a382244fc0a46a637950d163e25591a3
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-08 01:55:29 +0000

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A class Student that defines a student
+"""
+
+
+class Student:
+    """class body"""
+
+    def __init__(self, first_name, last_name, age):
+        """method body"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """method body"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """ method body """
+        if attrs is None:
+            return self.__dict__
+        return {
+                key: value for key, value in self.__dict__.items()
+                if key in attrs
+                }
+
+    def reload_from_json(self, json):
+        """ method body """
+        for key, value in json.items():
+            setattr(self, key, value)
