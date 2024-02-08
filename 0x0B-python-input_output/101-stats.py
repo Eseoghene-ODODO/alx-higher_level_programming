a7beb1a5bde890a281cb4fe6d33a39b3c28ff5f0
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-08 10:10:46 +0000

    101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,41 +4,44 @@ A script the reads stdin line by line and computes metrics
 """
 
 import sys
-import signal 
+import signal
 
 
 total_size = 0
 counts = {str(code): 0 for code in range(200, 506, 100)}
 
+
 def print_stats():
     """function body"""
-    global total_size, counts  
+    global total_size, counts
     print(f"File size: {total_size}")
     for code, count in sorted(counts.items()):
         if count > 0:
             print(f"{code}: {count}")
 
+
 def handler(signum, frame):
     """function body"""
     print_stats()
+
 
 signal.signal(signal.SIGINT, handler)
 
 i = 0
 for line in sys.stdin:
-    i += 1  
+    i += 1
     if i % 10 == 0:
         print_stats()
 
     parts = line.split()
 
     # Print for debugging
-    print(line) 
+    print(line)
     print(parts)
 
     size = int(parts[-2])
 
-    # Convert code to string explicitly 
+    # Convert code to string explicitly
     code = str(parts[-3])
 
     # Check if code is valid
