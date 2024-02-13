9dce69678ecaa850951cf74f9e58b5e79bf1a741
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-13 12:41:52 +0000

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,10 +13,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """method body"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -26,12 +26,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter body"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('width must be an integer')
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -41,12 +40,11 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter body"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('height must be an integer')
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be  > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -56,12 +54,11 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setter body"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('x must be an integer')
-        elif value <= 0:
+        if value < 0:
             raise ValueError('x must be >= 0')
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -71,12 +68,11 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter body"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('y must be an integer')
-        elif value <= 0:
+        if value < 0:
             raise ValueError(f'y must be >= 0')
-        else:
-            self.__y = value
+        self.__y = value
 
     def area(self):
         """area body"""
