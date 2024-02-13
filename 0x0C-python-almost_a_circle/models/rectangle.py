771a3e6d19453b41fae9bcb395e124ec1be74c4b
Eseoghene-ODODO <en.ododo@gmail.com>
2024-02-13 13:00:50 +0000

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
     def __str__(self):
         """method body"""
         return (f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - '
-               f' {self.__width}/{self.__height}')
+                f' {self.__width}/{self.__height}')
 
     def update(self, *args, **kwargs):
         """update method"""
