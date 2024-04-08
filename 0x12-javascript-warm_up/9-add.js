6d475c5439b61a94c88f347e50abe6c12e082fbb
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-08 22:33:18 +0100

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -10,7 +10,7 @@ const process = require('process');
 const args = process.argv;
 const num1 = parseInt(args[2]);
 const num2 = parseInt(args[3]);
-if ((isNaN(num1) || isNaN(num2)) && (num1 === undefined | num2 === undefined)) {
+if ((isNaN(num1) || isNaN(num2)) && (num1 === undefined || num2 === undefined)) {
   console.log(NaN);
 } else {
   console.log(add(num1, num2));
