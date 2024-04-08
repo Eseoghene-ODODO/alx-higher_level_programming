2d809095abd78122fca02972b27be8da92714946
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-08 16:52:36 +0000

    task 0-2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * A script that prints the following 3 lines:
+ * C is fun
+ * Python is cool
+ * Javascript  is  amazing
+ */
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("Javascript is amazing");
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+ * A script that prints a message depending on yhe number  of arguments passed.
+ */
+
+const process = require('process');
+let args = process.argv
+if (args.length <= 2)
+{
+	console.log("No argument");
+}
+else if (args.length === 3)
+{
+	console.log("Argument found");
+}
+else
+{
+	console.log("Arguments found");
+}
