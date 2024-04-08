d08f6aeab86467f0fd28613b571c2cdb4a4792c9
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-08 20:38:08 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@
 
 const process = require('process');
 const args = process.argv;
-if (args[2] === undefined) {
+if (isNaN(args[2])) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(args[2]));
