6fe894f80262f9b5e5f6c1474174d29e9364cfa6
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-08 18:02:10 +0100

    task 0-2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -6,6 +6,6 @@
  * Javascript  is  amazing
  */
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,16 +4,11 @@
  */
 
 const process = require('process');
-let args = process.argv
-if (args.length <= 2)
-{
-	console.log("No argument");
-}
-else if (args.length === 3)
-{
-	console.log("Argument found");
-}
-else
-{
-	console.log("Arguments found");
+const args = process.argv;
+if (args.length <= 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
